5af9e5d979d53875d51273265f29a46b7ea45941
NainaniJatinZ <nainani.jatin.0@gmail.com>
2022-06-08 14:42:51 +0530

    Forms

---
+++ django/basicforms/basicapp/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from django.core import validators
+
+class FormName(forms.Form):
+    name = forms.CharField()
+    email = forms.EmailField()
+    # verify_email = forms.EmailField(label='Enter your email again:')
+    text = forms.CharField(widget=forms.Textarea)
